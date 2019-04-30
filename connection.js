3be982fffcb704c0552a9b1dc4807c8a5c7e8f3e
Jays-Code <dfbfbc@gmail.com>
2019-04-30 10:22:01 -0400

    getting seeds to display, cleaning foreach statements

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-  mongoose.connect('mongodb://localhost/sampleBuddy');
+  mongoose.connect('mongodb://localhost/BuddySample');
 }
 mongoose.connection.on('error', function (err) {
   console.error('MongoDB connection error: ' + err);
--- seeds.js
+++ seeds.js
@@ -1,19 +1,6 @@
 // Connect to database
-let mongoose = require('mongoose');
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-    mongoose.connect('mongodb://localhost/SampleBuddy');
-}
-mongoose.connection.on('error', function (err) {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-}
-);
-mongoose.connection.once('open', function () {
-    console.log("Mongoose has connected to MongoDB!");
-});
+let mongoose = require('../SampleBuddy/connection.js');
+
 
 let Genre = require('./models/Genre.js')
 
